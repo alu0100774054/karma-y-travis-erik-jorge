8f8aa8966b5c9f666770c0bce94d5fa0ad9bd83b
alu0100774054 <alu0100774054@ull.edu.es>
2016-03-16 23:57:35 +0000

    arreglando tests

--- assets/js/temperatura_test.js
+++ assets/js/temperatura_test.js
@@ -1,6 +1,6 @@
 var expect = chai.expect;
 
-describe("Conversor", function () {
+describe("Medida", function () {
   describe("Constructor de la clase Medida", function() {
       it("Debería tener un Constructor", function() {
         var medida = new Medida(1, 'f');
@@ -12,4 +12,18 @@ describe("Conversor", function () {
         expect(medida.tipo).to.equal('c');
     });
   })
+});
+
+describe("Clase Temperatura", function () {
+  describe("constructor temperatura", function() {
+    it("deberia tener un constructor", function() {
+      var temperatura = new Temperatura (32, 'c');
+      expect(temperatura.valor).to.equal(32);
+    });
+    it("deberia poder recibir un solo parametro", function() {
+      var temperatura = new Temperatura ('32 c');
+      expect(temperatura.valor).to.equal(32);
+      expect(temperatura.tipo).to.equal('c');
+    });
+  });
 });
