cf5a2092836ec58a764d9fec8e42b702f7596543
alu0100774054 <alu0100774054@ull.edu.es>
2016-03-21 11:43:53 +0000

    cambiando gulp y test sin sentido

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,25 @@
-var gulp = require('gulp');
-var minify = require('gulp-minify');
-var cleanCSS = require('gulp-clean-css');
-var htmlmin = require('gulp-htmlmin');
-var clean = require('gulp-clean');
+var gulp    = require('gulp'),
+    gutil   = require('gulp-util'),
+    uglify  = require('gulp-uglify'),
+    concat  = require('gulp-concat');
+var del     = require('del');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS  = require('gulp-minify-css');
 
-gulp.task('minify-js', function() {
-  gulp.src(['vendor/*.js', '*.js', 'assets/js/*.js'])
-    .pipe(minify({
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '-min.js']
-    }))
-    .pipe(gulp.dest('minified'))
+gulp.task('minify', function () {
+  gulp.src('temperature.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+
+  gulp.src('./index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./minified/'))
+
+  gulp.src('./*.css')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/'))
 });
 
-gulp.task('minify-css', function() {
-  return gulp.src(['assets/css/*.css','vendor/*.css'])
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('minified'));
-})
-
-gulp.task('minify-html', function() {
-  return gulp.src('*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('minified'))
-});
-
-gulp.task('clean', function () {
-	return gulp.src('minified/*', {read: false})
-		.pipe(clean());
+gulp.task('clean', function(cb) {
+  del(['minified/*'], cb);
 });
