f53e14990f5be6d93f2540bf2df8bd7c91cb6d17
alu0100774054 <alu0100774054@ull.edu.es>
2016-03-21 10:49:15 +0000

    modificado gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,31 @@
-var gulp    = require('gulp'),
-    gutil   = require('gulp-util'),
-    uglify  = require('gulp-uglify'),
-    concat  = require('gulp-concat');
-var del     = require('del');
-var minifyHTML = require('gulp-minify-html');
-var minifyCSS  = require('gulp-minify-css');
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+var clean = require('gulp-clean');
 
-gulp.task('minify', function () {
-
-  gulp.src('conversor.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('minified'));
-
-  gulp.src('medida.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('minified'));
-
-  gulp.src('temperatura.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('minified'));
-
-  gulp.src('./index.html')
-    .pipe(minifyHTML())
-    .pipe(gulp.dest('./minified/'))
-
-  gulp.src('./*.css')
-   .pipe(minifyCSS({keepBreaks:true}))
-   .pipe(gulp.dest('./minified/'))
+gulp.task('minify-js', function() {
+  gulp.src(['vendor/*.js', '*.js', 'assets/js/*.js'])
+    .pipe(minify({
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('minified'))
 });
 
-gulp.task('clean', function(cb) {
-  del(['minified/*'], cb);
+gulp.task('minify-css', function() {
+  return gulp.src(['assets/css/*.css','vendor/*.css'])
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('minified'));
+})
+
+gulp.task('minify-html', function() {
+  return gulp.src('*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('minified'))
+});
+
+gulp.task('clean', function () {
+	return gulp.src('minified/*', {read: false})
+		.pipe(clean());
 });
